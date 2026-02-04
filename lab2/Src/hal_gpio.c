b8d8c7632e7611c5ba0abb7ffca53207178d7e2e
Charles Jones <charlesj24240@gmail.com>
2026-02-03 18:25:49 -0700

    Add lab1 GPIO setup to set pins and flash LED.

---
+++ lab2/Src/hal_gpio.c
@@ -0,0 +1,79 @@
+#include <stdint.h>
+#include <stm32f0xx_hal.h>
+#include <stm32f0xx_hal_gpio.h>
+
+void My_HAL_GPIO_Init(GPIO_TypeDef  *GPIOx, GPIO_InitTypeDef *GPIO_Init)
+{
+    (void)GPIO_Init;
+
+    if (GPIOx == GPIOC){
+        // set MODER
+        GPIOC->MODER &= ~(3u << (2*6) | 3u << (2*7) | 3u << (2*9));
+        GPIOC->MODER |= ((1u << (2*6)) | (1u << (2*7)) | (1u << (2*9)));
+
+        // set OTYPER
+        GPIOC->OTYPER &= ~(1u << (6) | 1u << (7) | 1u << (9));
+
+        // set OSPEEDR
+        GPIOC->OSPEEDR &= ~(3u << (2*6) | 3u << (2*7) | 3u << (2*9));
+
+        // set PUDR
+        GPIOC->PUPDR &= ~(3u << (2*6) | 3u << (2*7) | 3u << (2*9));
+    }
+
+    if (GPIOx == GPIOA) {
+
+    // PA0 input mode (MODER = 00)
+    GPIOA->MODER &= ~(3u << (2*0));
+
+    // Low speed (OSPEEDR = 00)
+    GPIOA->OSPEEDR &= ~(3u << (2*0));
+
+    // Pull-down resistor (PUPDR = 10)
+    GPIOA->PUPDR &= ~(3u << (2*0));
+    GPIOA->PUPDR |=  (2u << (2*0));
+    }
+}
+
+/*
+void My_HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
+{
+}
+*/
+
+
+GPIO_PinState My_HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{
+    if (GPIOx->IDR & GPIO_Pin) {
+        return GPIO_PIN_SET;
+    } else {
+        return GPIO_PIN_RESET;
+    }
+}
+
+
+void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
+{
+    if (PinState == GPIO_PIN_SET) {
+        GPIOx->ODR |= GPIO_Pin;     // set pin high
+    } else {
+        GPIOx->ODR &= ~GPIO_Pin;    // set pin low
+    }
+}
+
+
+void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{
+    GPIOx->ODR ^= GPIO_Pin;
+}
+
+
+void My_HAL_RCC_GPIOC_CLK_Enable(void)
+{
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+}
+
+void My_HAL_RCC_GPIOA_CLK_Enable(void)
+{
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+}
--- lab2/Src/lab2.c
+++ lab2/Src/lab2.c
@@ -14,11 +14,22 @@ int main(void)
   /* Configure the system clock */
   SystemClock_Config();
 
+  My_HAL_RCC_GPIOC_CLK_Enable(); // Enable the GPIOC clock
+
+  GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_9,
+                              GPIO_MODE_OUTPUT_PP,
+                              GPIO_SPEED_FREQ_LOW,
+                              GPIO_NOPULL};
+  My_HAL_GPIO_Init(GPIOC, &initStr); // Initialize pins PC6 & PC7
+
+  // Set green LED (PC9) high
+  My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+
   while (1)
   {
- 
+    My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
+    HAL_Delay(500);
   }
-  return -1;
 }
 
 /**
